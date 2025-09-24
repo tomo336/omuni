1402cf71e094aa52b8abf53d0391d73bb7d27563
tomo336 <rr25060q@st.omu.ac.jp>
2025-09-24 12:52:52 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,18 @@ int main(){
   {
    //もしpcから通信が来たら
    if(pc.readable()){
-   int n = pc.read(buf, sizeof(buf) - 1);//サイズ空け
+   int n = pc.read(buf, sizeof(buf) - 1);//末端のためのサイズ空け
    if (n > 0)  
     {
-    buf[n] = '\0'; //ヌル文字
+    buf[n] = '\0'; //ヌル文字(末端文字)
      if(buf[n-1] == '\n' || buf[n-1] =='\r'){
      buf[n-1] = '\0'; 
      number = atoi(buf); // 受信した文字列を整数に変換
      }
+      float x_in = 0, y_in = 0;//受信した数値をpwm配列に格納
+      sscanf(buf, "%f %f", &x_in, &y_in); // 受信した文字列を2つの浮動小数点数に変換
+      X = x_in;
+      Y = y_in;
     }
    }
    ThisThread::sleep_for(chrono::milliseconds(10));  
@@ -37,15 +41,14 @@ int main(){
     float value = hypot(X, Y);
 
     //オムニホイールの出力
-    pwm[0] = sin((rad-45)*M_PI/180.0f)*value;
-    pmw[1] = sin((rad-135)*M_PI/180.0f)*value;
-    pwm[2] = sin((rad-225)*M_PI/180.0f)*value;
-    pwm[3] = sin((rad-315)*M_PI/180.0f)*value;
-
+    pwm[0] = sin((rad-M_PI/4)*value);
+    pmw[1] = sin((rad-3*M_PI/4)*value);
+    pwm[2] = sin((rad+3*M_PI/4)*value);
+    pwm[3] = sin((rad+M_PI/4)*value);
+    
     
     //CAN通信
     CANMessage msg(2, (const uint8_t *)pwm, 8); //特に理由がない限りwhile直下
     can.write(msg); //特に理由がない限りwhile直下
  }
 }
-
