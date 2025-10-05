49ff17b26cae94f325526e356ba7f55cfa3ab415
tomo336 <rr25060q@st.omu.ac.jp>
2025-10-06 07:16:35 +0900

    Update PS4ReceiveData.ino

--- src/PS4ReceiveData.ino
+++ src/PS4ReceiveData.ino
@@ -15,14 +15,14 @@ void loop()
     if (PS4.LStickX()) 
     {
       int8_t coord1 = PS4.LStickX();
-      String str1 = String(static_cast<int>(coord1)+256);
+      String str1 = String(static_cast<int>(coord1));
       Serial.println(str1); //strに改行を付け加え、出力している
     }
 
     if (PS4.LStickY()) 
     {
     int8_t coord2 = PS4.LStickY();
-    String str2 = String(static_cast<int>(coord2));
+    String str2 = String(static_cast<int>(coord2)+256);
     Serial.println(str2);
     }
 
